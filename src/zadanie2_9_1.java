781ff9e6ba3f1a686c47e5229622369878bfecf4
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-19 01:33:37 +0300

    Initial commit

---
+++ src/zadanie2_9_1.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class zadanie2_9_1 {
+    // 2. Используя System.in (без сканнера), считайте слово из 5 букв.
+    public static void main(String[] args) throws IOException {
+        char x = (char)System.in.read();
+        char x2 = (char)System.in.read();
+        char x3 = (char)System.in.read();
+        char x4 = (char)System.in.read();
+        char x5 = (char)System.in.read();
+
+        System.out.println("Ответ на задание 2: Пример в коде.");
+    }
+}
---
+++ src/zadanie3_9_1.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.*;
+
+public class zadanie3_9_1 {
+    // 3. Используя System.in (без сканнера),считайте слово до пробела. Макс размер слова 10 символов.
+    public static void main(String[] args) throws IOException {
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите строку с, как минимум, одним пробелом, например 'We love java':");
+        String text = buff.readLine();
+        String words[] = text.split(" ");
+
+        buff.close();
+        System.out.println("Выводим (считываем) слово до пробела:");
+        System.out.println(words[0]);
+    }
+}
