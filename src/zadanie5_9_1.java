af876300c4c2e13420786bf22b79551f2e3f2499
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-20 00:44:26 +0300

    Initial commit

--- src/zadanie5_9_1.java
+++ src/zadanie5_9_1.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 // инфо по классу BufferedReader здесь: https://javarush.com/groups/posts/1919-schitihvanie-s-klaviaturih--riderih
+// инфо также здесь: https://youtu.be/TR8-chtKXFQ
+// и также здесь: https://youtu.be/-RiS_nFl9ok
 public class zadanie5_9_1 {
     /* 5. Пользователь вводит 7 символов. Используя System.in (без сканнера), считайте целое число
     до первой нецифры.(пример: «35 руб», ответ 35.) считаем, что первым пользователь всегда вводит
